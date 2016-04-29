b1645eaee7fcdcf099fe37b29051b164d832fa9d
Stacey Reiman <mspanish@zoho.com>
2016-04-29 14:28:59 -0800

    added some style and buttons to run algos

--- 2_week9/shroud_algos.js
+++ 2_week9/shroud_algos.js
@@ -8,12 +8,30 @@ var binaryArray = [];
 var array2 = [];
 var searchTerms =  ['heaven', 'resurrection', 'jesus'];
 var bibleNotation = ['1.3', '23.1', '14.8'];
-var fileNumber = '4';
+var fileNumber = '1';
+var fileNumberArray = _.range([start=1], 20, [step=1]);
 
+$(document).ready(function($){
+  // when our document is loaded, add this action to the first button
+  $('#btn1').click(function(){
+    runFile(fileNumber);
+  });
+  // when our document is loaded, add this action to the second button
+  $('#btn2').click(function(){
+    // loop through EACH of the numbers between 1 and 20 in our fileNumberArray, and extract binary code for that file
+    $.each(fileNumberArray,function(index, value){
+      setTimeout(function(){
+        runFile(value);
+      }, 300);
+    })
+  });  
+});
+
+function runFile(file) {
 // here we use a call to get the text file which has the code behind the image file in a text format
 
 $.ajax({
-  url: 'https://raw.githubusercontent.com/HolyRosaryAcademy/fall2015/master/2_week9/txts/'+fileNumber+'.txt',
+  url: 'https://raw.githubusercontent.com/HolyRosaryAcademy/fall2015/master/2_week9/txts/'+file+'.txt',
   type: 'GET',
   dataType: 'text',
    success: function (res) {
@@ -29,21 +47,22 @@ $.ajax({
      // Algorithm #1:  createBinaryArray
      
      // here we are just going to cycle through every character in this data and look for pairs of 00, 10, or 01. If you find such a pair, push it into our binaryArray.
-    createBinaryArray();
+    createBinaryArray(file);
    },
    error: function () {
        console.log('sorry, could not get your Shroud text');
    }
 });
+}; // end runFile() function
 
-function createBinaryArray() {
+function createBinaryArray(file) {
 // we have to split this large chunk of text into an array of individual character sets
  binaryArray =  binaryRawData.split(" ");
  // now we are ready to check the characters for 0s and 1s
- parseBinaryArray();
+ parseBinaryArray(file);
 }
 
-function parseBinaryArray() {
+function parseBinaryArray(file) {
     // this is a simple loop, using the Javascript helper library known as jQuery. It creates a LOOP for us, looping over the binaryRawData data and adding each individual character that is a 1 or a 0 to an ordered array, 
  $.each(binaryArray, function(index, value){
 
@@ -57,10 +76,10 @@ function parseBinaryArray() {
    }
  
    if (index == (binaryArray.length-1)) {
-    console.log('our final array is '+array2);
+   // console.log('our final array is '+array2);
     var result = JSON.stringify(array2.join(''))
     result = result.replace(/"/g, '');
-    console.save(result, 'shroud_binary_'+fileNumber+'.txt')
+    console.save(result, 'shroud_binary_'+file+'.txt')
    }
  });
 
