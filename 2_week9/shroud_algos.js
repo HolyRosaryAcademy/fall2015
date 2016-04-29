026865ca9bdc39525abbb5e54b83d7ff9efaf428
Stacey Reiman <mspanish@zoho.com>
2016-04-29 10:50:40 -0800

    upd

--- 2_week9/shroud_algos.js
+++ 2_week9/shroud_algos.js
@@ -1,10 +1,11 @@
 // we'll store our Shroud data in this Javascript object first
-var binaryRawData = {};
+var binaryRawData = '';
 
 // now we'll set up some arrays we'll need to store and parse our data about the Shroud
 
 var charactersArray = [];
 var binaryArray = [];
+var array2 = [];
 var searchTerms =  ['heaven', 'resurrection', 'jesus'];
 var bibleNotation = ['1.3', '23.1', '14.8'];
 
@@ -36,12 +37,29 @@ $.ajax({
 });
 
 function createBinaryArray() {
-  // this is a simple loop, using the Javascript helper library known as jQuery. It creates a LOOP for us, looping over the binaryRawData data and adding each individual character that is a 1 or a 0 to an ordered array, 
-  
- // $.each(binaryRawData, function(index, value){
- //   console.log('my index is '+index);
- //   if (index == 10) {
- //    return false
- //   }
- // });
+// we have to split this large chunk of text into an array of individual character sets
+ binaryArray =  binaryRawData.split(" ");
+ // now we are ready to check the characters for 0s and 1s
+ parseBinaryArray();
+}
+
+function parseBinaryArray() {
+    // this is a simple loop, using the Javascript helper library known as jQuery. It creates a LOOP for us, looping over the binaryRawData data and adding each individual character that is a 1 or a 0 to an ordered array, 
+ $.each(binaryArray, function(index, value){
+
+  if (value == '00' || value == '01' || value == '10' || value =='11') {
+    array2.push(value);
+  }
+   // let's take a look at the data in our console, just the first 10 so we don't run into memory problems!
+   if (index < 10) {
+   console.log('my index is '+index);
+   console.log('my value is '+value);
+   }
+ 
+   if (index == (binaryArray.length-1)) {
+    console.log('our final array is '+array2);
+    console.save(array2)
+   }
+ });
+
 }
